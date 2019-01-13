f522dd7aaa932b03aa2c3916c1b082a19317f5d1
lindsey0307 <lindseypoad@gmail.com>
2019-01-13 16:52:45 -0500

    add enemy death logic and more animations

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -6,7 +6,7 @@ public class BallController : MonoBehaviour {
 
 	public float speed = 10f;
 	// Use this for initialization
-	void Start () {
+    void Start () {
 		GetComponent<Rigidbody2D>().velocity = Vector2.left * speed;
 	}
 	
@@ -16,6 +16,7 @@ public class BallController : MonoBehaviour {
 	}
 
     void OnCollisionEnter2D(Collision2D col){
+
         float y = HitFactor(transform.position, col.transform.position, col.collider.bounds.size.y);
         Vector2 dir = new Vector2(1, y).normalized;
         GetComponent<Rigidbody2D>().velocity = dir * speed;
--- Assets/Scripts/PaddleController.cs
+++ Assets/Scripts/PaddleController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PaddleController : MonoBehaviour {
 
 	public float speed;
-	private Vector3 playerPos = new Vector3(-6f, 0f, 0f);
+	private Vector3 playerPos = new Vector3(-6f, 0f, 0.5f);
 
 	// Use this for initialization
 	void Start () {
@@ -15,7 +15,7 @@ public class PaddleController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		float yPos = transform.position.y + (Input.GetAxis("Vertical") * speed);
-		playerPos = new Vector3(-6f, Mathf.Clamp(yPos, -3.67f, 3.76f), 0);
+		playerPos = new Vector3(-6f, Mathf.Clamp(yPos, -4.23f, 4.27f), 0.5f);
 		transform.position = playerPos;
 	}
 }
